f2850c459e87abed0cf907af20fad9d5a7226af4
JacquesMeiring <meirj1@student.op.ac.nz>
2025-05-12 16:24:28 +1200

    Added three new Areas, Starting Area, Random Next Area, and the Ending Area.

--- Program.cs
+++ Program.cs
@@ -11,20 +11,31 @@
             Console.WriteLine("The Menu options are:");
             Console.WriteLine("1  New Game");
             Console.WriteLine("2  Options");
-            Console.WriteLine();
             Console.WriteLine("0  Exit menu system");
+            Console.WriteLine();
         }
 
         static void Task1()
         {
-            // NEW GAME CREATION --> Lead to Character Creation and Start of Story
+            // NEW GAME CREATION --> Lead to Start of Story and Character Creation
 
-            Console.WriteLine("This is Task 1");
+            StoryLineSum();
 
+        }
+
+        static void StoryLineSum()
+        {
+            // Print to Screen Main Story Summary?
+
+            Console.WriteLine("This is the main story");
+
+            Console.WriteLine("Press any key to go to Character Creation");
+            Console.ReadLine();
+
+
+            // Lead to Character Creation?
             CharacterCreation();
 
-            Console.WriteLine("Press any key to return to menu");
-            Console.ReadLine();
 
         }
 
@@ -33,11 +44,50 @@
         {
             // List Character Options??
 
-            Console.WriteLine();
+            Console.WriteLine("This is where you can choose or create your Character");
 
-            // Print to Screen Main Story Summary?
+
+            Console.WriteLine("Press any key to move on to the Starting Area");
+            Console.ReadLine();
+
+
+
+
+
+
+            // Lead to first Starting Area?
+
+            StartingArea();
 
         }
+
+        static void StartingArea()
+        {
+            Console.WriteLine("This is the Starting Area");
+            Console.WriteLine("Press any key to move on to the next area...");
+            Console.ReadLine();
+            RandomNextArea();
+        }
+
+        static void RandomNextArea()
+        {
+            Console.WriteLine("This is the second explorable Area");
+            Console.WriteLine("Press any key to go to the END");
+            Console.ReadLine();
+            EndArea();
+
+        }
+
+        static void EndArea()
+        {
+            Console.WriteLine("Well done you have made it to the end");
+            Console.WriteLine("Press any key to go back to the main Menu");
+            Console.ReadLine();
+
+        }
+
+
+
 
 
         static void EpicLoot()
@@ -51,6 +101,10 @@
             string[] explosives;
 
         }
+
+
+
+
 
 
             static void Task2()
