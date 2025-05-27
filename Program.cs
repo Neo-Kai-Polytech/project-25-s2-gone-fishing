efe87eb7428fe5cdd0bb68212f113c633f816e95
LiamVisser <VISSLC1@student.op.ac.nz>
2025-05-27 12:50:49 +1200

    Added Varenniki recipie

--- Program.cs
+++ Program.cs
@@ -1198,6 +1198,36 @@ namespace UberProject
                     }
                 }
             }
+            else if (ingredient1 == "cheese" && ingredient2 == "potato" && ingredient3 == "eggs")
+            {
+                for (int i = 0; i < inventory.Length; i++)
+                { //Adds Varenniki
+                    if (inventory[i] == "Cheese")
+                    {
+                        inventory[i] = "Varenniki";
+                        Console.WriteLine("You made Varenniki");
+                        for (int j = 0; j < inventory.Length; j++) //Removes second ingredient
+                        {
+                            if (inventory[j] == "Potato")
+                            {
+                                inventory[j] = "";
+
+                                for (int k = 0; k < inventory.Length; k++) //Removes third ingredient
+                                {
+                                    if (inventory[k] == "Eggs")
+                                    {
+                                        inventory[k] = "";
+
+                                        break;
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
             else if (ingredient1 == "pickle" && ingredient2 == "lemon" && ingredient3 == "flour")
             {
                 for (int i = 0; i < foodSatchel.Length; i++)
