4cbcfd16de74387c5638899d43687cce790c4f0d
JacquesMeiring <meirj1@student.op.ac.nz>
2025-05-29 15:03:03 +1200

    Added new Arena Mode

--- Program.cs
+++ Program.cs
@@ -124,7 +124,7 @@ namespace UberProject
             Console.ResetColor();
             Console.WriteLine("The Menu options are:");
             Console.WriteLine("1  New Game");
-            Console.WriteLine("2  Options");
+            Console.WriteLine("2  Arena");
             Console.WriteLine("3  Collection");
             Console.WriteLine("0  Exit menu system");
             Console.Beep(659, 200); // E5
@@ -949,47 +949,34 @@ namespace UberProject
                 }
             }
 
-            Console.WriteLine($"You have received {acquiredIngredient.ingredientName}");
-            Thread.Sleep(1000);
-            Console.WriteLine();
-            for (int i = 0; i < foodSatchel.Length; i++)
-            {
-                if (foodSatchel[i] == "")
-                {
-                    foodSatchel[i] = acquiredIngredient.ingredientName;
-                    break;
-                }
-            }
-
-
-
             Console.WriteLine("Press any button to continue...");
             Console.ReadLine();
 
         }
 
-        public static Enemies RadndomHostileEncounter()
+        public static Enemies RadndomHostileEncounter(int i)
         {
             // This will be an Array of Enemies that you can randomly encounter in the game.
 
+            int modifier = i * 2;
 
             Enemies[] monsters =
         {
-            new Enemies("Mirelurk", 30, 5, "A mutated crab-like creature with a hard shell and a vicious bite."),
-            new Enemies("Super Mutant", 40, 8, "A hulking brute with enhanced strength and resilience, often armed with heavy weapons."),
-            new Enemies("Deathclaw", 50, 10, "A terrifying predator with razor-sharp claws and incredible speed."),
-            new Enemies("Radscorpion", 35, 7, "A giant scorpion with a venomous sting and armored exoskeleton."),
-            new Enemies("Radroach", 20, 4, "Giant irradiated cockroach with acidic saliva and armored carapace"),
-            new Enemies("Mutant Hound", 35, 7, "Once a domestic dog, now a grotesque, hairless predator with enhanced senses and aggression."),
-            new Enemies("Ash Crawler", 25, 5, "A pale, skeletal creature that burrows through radioactive ash, ambushing prey from below."),
-            new Enemies("Ghoul Reaper", 30, 6, "A sentient, decaying humanoid that hunts the living for flesh, often in packs."),
-            new Enemies("Wasteland Abomination", 50, 10, "A massive fusion of flesh and machinery, created from failed experiments and scavenged tech."),
-            new Enemies("Irradiated Behemoth", 60, 12, "Towering brute pulsing with nuclear energy, leaving scorched earth in its wake."),
-            new Enemies("Scorched Stalker", 28, 6, "Burn-scarred humanoid with smoldering skin, capable of setting traps and ambushes."),
-            new Enemies("Toxic Spitter", 22, 5, "A bloated mutant that expels corrosive bile from a distended jaw."),
-            new Enemies("Bone Dragger", 26, 5, "Lurks in the shadows, dragging scavenged bones across the ground to lure victims."),
-            new Enemies("Nuke Leech", 18, 3, "A glowing, slug-like parasite that feeds on radiation and attaches to living hosts."),
-            new Enemies("Murderous Crow", 7, 10, "A seemingly normal crow with teeth that can cut through ceramic plates")
+            new Enemies("Mirelurk", 4*modifier, 7, "A mutated crab-like creature with a hard shell and a vicious bite."),
+            new Enemies("Super Mutant",3*modifier , 8, "A hulking brute with enhanced strength and resilience, often armed with heavy weapons."),
+            new Enemies("Deathclaw", 5*modifier, 10, "A terrifying predator with razor-sharp claws and incredible speed."),
+            new Enemies("Radscorpion", 2*modifier, 7, "A giant scorpion with a venomous sting and armored exoskeleton."),
+            new Enemies("Radroach", 2*modifier, 4, "Giant irradiated cockroach with acidic saliva and armored carapace"),
+            new Enemies("Mutant Hound", 3*modifier, 7, "Once a domestic dog, now a grotesque, hairless predator with enhanced senses and aggression."),
+            new Enemies("Ash Crawler", 2*modifier, 5, "A pale, skeletal creature that burrows through radioactive ash, ambushing prey from below."),
+            new Enemies("Ghoul Reaper", 3*modifier, 6, "A sentient, decaying humanoid that hunts the living for flesh, often in packs."),
+            new Enemies("Wasteland Abomination", 5*modifier, 10, "A massive fusion of flesh and machinery, created from failed experiments and scavenged tech."),
+            new Enemies("Irradiated Behemoth", 6*modifier, 11, "Towering brute pulsing with nuclear energy, leaving scorched earth in its wake."),
+            new Enemies("Scorched Stalker", 3*modifier, 6, "Burn-scarred humanoid with smoldering skin, capable of setting traps and ambushes."),
+            new Enemies("Toxic Spitter", 2*modifier, 5, "A bloated mutant that expels corrosive bile from a distended jaw."),
+            new Enemies("Bone Dragger", 3*modifier, 5, "Lurks in the shadows, dragging scavenged bones across the ground to lure victims."),
+            new Enemies("Nuke Leech", 2*modifier, 5, "A glowing, slug-like parasite that feeds on radiation and attaches to living hosts."),
+            new Enemies("Murderous Crow", 2*modifier, 4, "A seemingly normal crow with teeth that can cut through ceramic plates")
         };
 
             int enemyNumber = random.Next(monsters.Length);
@@ -999,13 +986,54 @@ namespace UberProject
         }
 
 
-        static void Task2()
+        static void Arena()
         {
-            // OPTIONS MENU
+            int i = 1;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Welcome to the Arena Maggot!");
+            Thread.Sleep(1000);
+            Console.WriteLine("This world ain't for the weak!");
+            Thread.Sleep(1000);
+            Console.WriteLine("Survive for as long as you can in the Arena and prove your worth!\n");
+            Thread.Sleep(1000);
 
-            Console.WriteLine("This is Task 2");
-            Console.WriteLine("Press any key to return to menu");
+            Console.WriteLine("Enter the name of your character.");
+            charName = Console.ReadLine();
+            Player player = new Player(charName, 100, "Steel Pan", 6);
+
+            Console.ResetColor();
+
+            Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
+
+            do 
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Round {i++}");
+                Console.ResetColor();
+                Thread.Sleep(2000);
+
+                Console.Clear();
+                Enemies enemy = RadndomHostileEncounter(i);
+                CombatSystem(ref player, enemy);
+                if (player.playerHP <= 0)
+                {
+                    Console.WriteLine("You have been defeated in the Arena. Game Over!");
+                    Console.WriteLine($"You survived for {i - 1} rounds.");
+                    Console.WriteLine("Press Enter to exit...");
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"You have defeated the {enemy.enemyName}!");
+                    Thread.Sleep(1000);
+                    AcquireEpicLoot();
+                }
+
+                Console.Clear();
+            } while (player.playerHP > 0);
+
 
         }
         public static void Task3()
@@ -1585,7 +1613,7 @@ namespace UberProject
                         break;
 
                     case 2:
-                        Task2();
+                        Arena();
                         break;
 
                     case 3:
